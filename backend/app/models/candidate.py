3036eadd0533890e2e7eb08befec3248ced7301b
Avaron23 <margievg04@gmail.com>
2026-02-11 22:17:08 +0300

    feat(models): add BaseModel and Candidate model with SQLAlchemy 2.0 typing

---
+++ backend/app/models/candidate.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Integer, String, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import ARRAY 
+from app.db.base import Base
+from typing import List
+
+
+class Candidate(Base):
+    __tablename__ = "candidates"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String(255), nullable=False)
+    experience: Mapped[int] = mapped_column(Integer)
+    education: Mapped[str] = mapped_column(String(50))
+    skills: Mapped[List[str]] = mapped_column(ARRAY(String))
+    resume_score: Mapped[int] = mapped_column(Integer)
+    interview_score: Mapped[int] = mapped_column(Integer)
+    tests_score: Mapped[int] = mapped_column(Integer)
+    desired_salary: Mapped[int] = mapped_column(Integer)
+    recomendation_score: Mapped[int] = mapped_column(Integer)
+    #created_at: Mapped[DateTime] = mapped_column(DateTime, nullable=False)  
