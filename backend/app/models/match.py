031a5b4efd362b07214126712dfd4644484250b3
Avaron23 <margievg04@gmail.com>
2026-02-13 01:04:11 +0300

    feat(models): added the Vacancy and Match sqlalchemy models

---
+++ backend/app/models/match.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import ARRAY 
+from app.db.base import Base
+from app.models.candidate import Candidate
+from app.models.vacancy import Vacancy
+from typing import List
+
+
+class Match(Base):
+    __tablename__ = "matches"
+
+    candidate_id: Mapped[int] = mapped_column(ForeignKey('candidates.id', ondelete='CASCADE'), primary_key=True)
+    vacancy_id: Mapped[int] = mapped_column(ForeignKey('vacancies.id', ondelete='CASCADE'), primary_key=True)
+    total_score: Mapped[int] = mapped_column(Integer)
+    matched_skills: Mapped[List[str]] = mapped_column(ARRAY(String))
---
+++ backend/app/models/vacancy.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import ARRAY 
+from app.db.base import Base
+from typing import List
+
+
+class Vacancy(Base):
+    __tablename__ = "vacancies"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    title: Mapped[str] = mapped_column(String(255), nullable=False)
+    required_experience: Mapped[int] = mapped_column(Integer)
+    required_education: Mapped[str] = mapped_column(String(50))
+    required_skills: Mapped[List[str]] = mapped_column(ARRAY(String))
+    min_resume_score: Mapped[int] = mapped_column(Integer)
+    min_interview_score: Mapped[int] = mapped_column(Integer)
+    min_tests_score: Mapped[int] = mapped_column(Integer)
+    salary_offer: Mapped[int] = mapped_column(Integer)
+    min_recomendation_score: Mapped[int] = mapped_column(Integer)
