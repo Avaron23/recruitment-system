31f4b3867efc35d94b0b13aa7e35daf0f2f287fd
Avaron23 <margievg04@gmail.com>
2026-02-12 22:59:59 +0300

    feat(db): add async SQLAlchemy engine and initialization scripts

--- backend/app/db/db.py
+++ backend/app/db/db.py
@@ -1,1 +1,16 @@
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from app.core.config import settings
+from typing import AsyncGenerator
+
+
+DATABASE_URL = settings.database_url
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
---
+++ backend/app/db/init_db.py
@@ -0,0 +1,16 @@
+import asyncio
+from app.db.db import engine
+from app.db.base import Base
+
+from app.models.candidate import Candidate
+from app.models.match import Match
+from app.models.vacancy import Vacancy
+
+
+async def init_models():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all())
+    print("Таблицы успешно созданы!")
+
+if __name__ == "__main__":
+    asyncio.run(init_models())
