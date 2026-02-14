e583373f90f80a106864ab1e89ab53e0724688ac
Avaron23 <margievg04@gmail.com>
2026-02-14 18:33:18 +0300

    feat(routers) connecting the candidates and vacancies routers in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
+from app.api.candidates import router as candidates_router
+from app.api.vacancies import router as vacancies_router
 
 
 app = FastAPI()
 
+app.include_router(candidates_router)
+app.include_router(vacancies_router)
 
 @app.get("/", tags=["Root"])
 async def root():
