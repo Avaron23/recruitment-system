cc0906ae594041e86d5edbf67d221a3c8492733e
Avaron23 <margievg04@gmail.com>
2026-04-22 18:37:01 +0300

    Revert "feat(frontend): add React frontend generated by AI"
    This reverts commit d08a7efdd8c15ba33624ef75b3167de8132a0a68.

--- frontend/src/api/matches.ts
+++
@@ -1,7 +0,0 @@
-import api from './client';
-import type { Match, MatchCreate } from '../types/match';
-
-export const matchesApi = {
-  getAll: () => api.get<Match[]>('/matches').then(res => res.data),
-  create: (data: MatchCreate) => api.post<Match>('/matches', data).then(res => res.data),
-};
--- frontend/src/api/vacancies.ts
+++
@@ -1,8 +0,0 @@
-import api from './client';
-import type { Vacancy, VacancyCreate } from '../types/vacancy';
-
-export const vacanciesApi = {
-  getAll: () => api.get<Vacancy[]>('/vacancies').then(res => res.data),
-  getById: (id: number) => api.get<Vacancy>(`/vacancies/${id}`).then(res => res.data),
-  create: (data: VacancyCreate) => api.post<Vacancy>('/vacancies', data).then(res => res.data),
-};
